9f9fd8becdaccdd8cd031c32e2ae37944d6a70ca
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2023-07-02 20:35:57 +0100

    Parity nearly all complete.

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,28 +2,43 @@ from test_framework import generic_test
 
 """ 4.1.1 Computing the Parity of a Word """
 
+from math import floor
+
 
 def compute_lookup_table(l: int) -> dict[str, int]:
     """
-    Computes a lookup table based on the passed-in length.
+    Computes a lookup table based on the passed-in mask_size.
 
-    Arguments:
-        l: The width of the word for which results are cached.
+    :arguments:
+        l: The mask size.
 
+    :returns:
+        A precomputed lookup table where each key is a bitword and the value its
+        parity.
     """
-    
+
     def _compute_parity(x: int) -> int:
-        """ Computes the parity of a word. """
-    
-    max_num = 2 ^ l
+        """Computes the parity of a word."""
+        result = 0
+        while x:
+            result ^= 1
+            x &= x - 1
+        return result
+
+    max_num = 2**l
     curr = 0
     table = {}
-    while curr <= max_num:
-        table[]
-    # while x:
-    #   result ^= 1
-    #   x &= x - 1
-    # return result
+
+    while curr < max_num:
+        table[curr] = _compute_parity(curr)
+        curr += 1
+
+    return table
+
+
+MAX_SIZE = 64
+MASK_SIZE = int(MAX_SIZE / 4)  # Each key in the lookup table is 16 bits
+LOOKUP_TABLE = compute_lookup_table(MASK_SIZE)
 
 
 def parity(x: int) -> int:
@@ -48,15 +63,34 @@ def parity(x: int) -> int:
     # - XOR to result.
     # - Drop the lowest set bit.
     #
-    result = 0
-    while x:
-      result ^= 1
-      x &= x - 1  # Drops the lowest set bit
-    return result
+    # result = 0
+    # while x:
+    #     result ^= 1
+    #     x &= x - 1  # Drops the lowest set bit
+    # return result
 
     # 3) Look up table - O(n/L) where n is the length of the word and L is the
     #                    the size of the window.
-    # i) Compute the lookup table.
+    # i)  Compute the lookup table.
+    # ii) Use look up table by calculating the parity of each non-overlapping
+    #     group
+    # bitmask = 0xFFFF  # F is 1111
+    # return (
+    #     LOOKUP_TABLE[x & bitmask]
+    #     ^ LOOKUP_TABLE[(x >> MASK_SIZE) & bitmask]
+    #     ^ LOOKUP_TABLE[(x >> (2 * MASK_SIZE)) & bitmask]
+    #     ^ LOOKUP_TABLE[(x >> (3 * MASK_SIZE)) & bitmask]
+    # )
+
+    # 4) Use associativity - O(log(n)) where n is the length of the word
+    # The XOR of a group of bits is its parity.
+    half = int(MAX_SIZE / 2)
+    while half > 0:
+        x ^= x >> half
+        half = floor(half / 2)
+        print(half)
+
+    return x & 0x1
 
 
 if __name__ == "__main__":
