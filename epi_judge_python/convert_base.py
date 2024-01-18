cb657a5b7beba1250a2afc5f3b5c812e58edc90b
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-01-18 08:37:30 +0000

    6.2 Convert base

--- epi_judge_python/convert_base.py
+++ epi_judge_python/convert_base.py
@@ -1,12 +1,40 @@
 from test_framework import generic_test
+import string, functools
 
 
 def convert_base(num_as_string: str, b1: int, b2: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    """Convert between bases."""
+
+    def construct_from_base(num_as_int, base):
+        """Use dynamic programming instead of recursion!"""
+        result = []
+
+        while num_as_int > 0:
+            result.append(string.hexdigits[num_as_int % base].upper())
+            num_as_int //= base
+
+        return "".join(result[::-1]) if result else "0"
+
+    # 1) Convert to a decimal base. This is the most basic and easiest one.
+    is_negative = False
+    if num_as_string[0] == "-":
+        is_negative = True
+
+    # functools.reduce is great for accumulation!
+    num_as_int = functools.reduce(
+        lambda x, c: x * b1 + string.hexdigits.index(c.lower()),
+        num_as_string[is_negative:],
+        0,
+    )
+
+    return ("-" if is_negative else "") + (
+        "0" if num_as_int == 0 else construct_from_base(num_as_int, b2)
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('convert_base.py', 'convert_base.tsv',
-                                       convert_base))
+        generic_test.generic_test_main(
+            "convert_base.py", "convert_base.tsv", convert_base
+        )
+    )
