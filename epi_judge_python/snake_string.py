81c0ded3e8a241550fd263ea321ba10242e3b1c9
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-01-21 13:15:42 +0000

    6.10

--- epi_judge_python/snake_string.py
+++ epi_judge_python/snake_string.py
@@ -1,12 +1,31 @@
 from test_framework import generic_test
 
+""" 6.10 """
+
 
 def snake_string(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    """Use periodicity to write a sinusodial string."""
+
+    result = []
+
+    # Output the first row.
+    for i in range(1, len(s), 4):
+        result.append(s[i])
+
+    # The second row!
+    for i in range(0, len(s), 2):
+        result.append(s[i])
+
+    # The third row.
+    for i in range(3, len(s), 4):
+        result.append(s[i])
+
+    return "".join(result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('snake_string.py', 'snake_string.tsv',
-                                       snake_string))
+        generic_test.generic_test_main(
+            "snake_string.py", "snake_string.tsv", snake_string
+        )
+    )
