98204f99b1b8b6fb027e6b57c859ab26dbb3bf12
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-02-08 19:33:25 +0000

    8.3

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -1,13 +1,30 @@
 from test_framework import generic_test
 
+""" 8.3 """
+
 
 def is_well_formed(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    """Checks if a string is well-formed."""
+    # This is best done using a stack!
+
+    left_chars = []
+    lookup_table = {"[": "]", "{": "}", "(": ")"}
+    for c in s:
+        if c in lookup_table:
+            # Left char. So store it.
+            left_chars.append(c)
+        else:
+            # Check right char and match it.
+            if not left_chars or lookup_table[left_chars.pop()] != c:
+                return False
+    return not left_chars
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('is_valid_parenthesization.py',
-                                       'is_valid_parenthesization.tsv',
-                                       is_well_formed))
+        generic_test.generic_test_main(
+            "is_valid_parenthesization.py",
+            "is_valid_parenthesization.tsv",
+            is_well_formed,
+        )
+    )
