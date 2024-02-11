5827c243d165c7c6ec5a3036a142b793955cb8f3
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-02-11 14:36:58 +0000

    8.8

--- epi_judge_python/queue_from_stacks.py
+++ epi_judge_python/queue_from_stacks.py
@@ -1,38 +1,49 @@
+from typing import List
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+""" 8.8 """
+
 
 class Queue:
+    def __init__(self) -> None:
+        self._enq: List[int] = []
+        self._deq: List[int] = []
+
     def enqueue(self, x: int) -> None:
-        # TODO - you fill in here.
-        return
+        self._enq.append(x)
 
     def dequeue(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        if not self._deq:
+            while self._enq:
+                self._deq.append(self._enq.pop())
+        return self._deq.pop()
 
 
 def queue_tester(ops):
     try:
         q = Queue()
 
-        for (op, arg) in ops:
-            if op == 'Queue':
+        for op, arg in ops:
+            if op == "Queue":
                 q = Queue()
-            elif op == 'enqueue':
+            elif op == "enqueue":
                 q.enqueue(arg)
-            elif op == 'dequeue':
+            elif op == "dequeue":
                 result = q.dequeue()
                 if result != arg:
-                    raise TestFailure('Dequeue: expected ' + str(arg) +
-                                      ', got ' + str(result))
+                    raise TestFailure(
+                        "Dequeue: expected " + str(arg) + ", got " + str(result)
+                    )
             else:
-                raise RuntimeError('Unsupported queue operation: ' + op)
+                raise RuntimeError("Unsupported queue operation: " + op)
     except IndexError:
-        raise TestFailure('Unexpected IndexError exception')
+        raise TestFailure("Unexpected IndexError exception")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('queue_from_stacks.py',
-                                       'queue_from_stacks.tsv', queue_tester))
+        generic_test.generic_test_main(
+            "queue_from_stacks.py", "queue_from_stacks.tsv", queue_tester
+        )
+    )
