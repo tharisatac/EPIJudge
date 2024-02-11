5eda3482791d465b8f5a82f2f0a28268d81390d1
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-02-11 13:45:30 +0000

    8.4

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -2,12 +2,37 @@ from test_framework import generic_test
 
 
 def shortest_equivalent_path(path: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    """Finds the shortest equivalent path."""
+
+    if not path:
+        raise ValueError()
+
+    path_names = []
+
+    if path[0] == "/":
+        # Absolute path
+        path_names.append("/")
+
+    for token in (token for token in path.split("/") if token not in [".", ""]):
+        if token == "..":
+            if not path_names or path_names[-1] == "..":
+                path_names.append(token)
+            else:
+                if path_names[-1] == "/":
+                    raise ValueError()
+                path_names.pop()
+        else:
+            # Must be a path
+            path_names.append(token)
+    result = "/".join(path_names)
+    return result[result.startswith("//") :]  # Avoids a triple slash start
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('directory_path_normalization.py',
-                                       'directory_path_normalization.tsv',
-                                       shortest_equivalent_path))
+        generic_test.generic_test_main(
+            "directory_path_normalization.py",
+            "directory_path_normalization.tsv",
+            shortest_equivalent_path,
+        )
+    )
