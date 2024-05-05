84d61a6724d568220718274b084c8a60bc0e3a43
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-05-05 14:46:58 +0100

    11.1

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -2,14 +2,30 @@ from typing import List
 
 from test_framework import generic_test
 
+""" 11.1 """
+
 
 def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    """Find the first k in the array."""
+
+    left, right, result = 0, len(A) - 1, -1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if A[mid] > k:
+            right = mid - 1
+        elif A[mid] == k:
+            result = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('search_first_key.py',
-                                       'search_first_key.tsv',
-                                       search_first_of_k))
+        generic_test.generic_test_main(
+            "search_first_key.py", "search_first_key.tsv", search_first_of_k
+        )
+    )
