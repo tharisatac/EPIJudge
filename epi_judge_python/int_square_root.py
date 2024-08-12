e488147cbc853fbd89cb20da611af315e87c4b38
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-08-12 18:42:54 +0100

    11.4

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -2,11 +2,25 @@ from test_framework import generic_test
 
 
 def square_root(k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    """Find the the largest integer whose square is equal to or smaller than k"""
+    left, right = 0, k
+
+    # Whilst there's an interval. It's less than or equal to because if it's
+    # equal, we can still keep going.
+    while left <= right:
+        m = (left + right) // 2
+        m_sq = m * m
+        if m_sq <= k:
+            left = m + 1
+        elif m_sq > k:
+            right = m - 1
+
+    return left - 1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('int_square_root.py',
-                                       'int_square_root.tsv', square_root))
+        generic_test.generic_test_main(
+            "int_square_root.py", "int_square_root.tsv", square_root
+        )
+    )
