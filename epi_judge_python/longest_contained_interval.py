c42330e88e6af898cb9eb92f8e5245262018d969
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-08-29 14:49:53 +0100

    12.9

--- epi_judge_python/longest_contained_interval.py
+++ epi_judge_python/longest_contained_interval.py
@@ -2,14 +2,43 @@ from typing import List
 
 from test_framework import generic_test
 
+""" 12.9 """
+
 
 def longest_contained_range(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    """Find the longest contained range"""
+
+    unprocessed_entries = set(A)
+
+    result = 0
+
+    while unprocessed_entries:
+        a = unprocessed_entries.pop()
+
+        # Find the lower bound
+        lower_bound = a - 1
+        while lower_bound in unprocessed_entries:
+            unprocessed_entries.remove(lower_bound)
+            lower_bound -= 1
+
+        # Find the upper bound
+        upper_bound = a + 1
+        while upper_bound in unprocessed_entries:
+            unprocessed_entries.remove(upper_bound)
+            upper_bound += 1
+
+        if upper_bound - lower_bound - 1 > result:
+            # - 1 as it's inclusive
+            result = upper_bound - lower_bound - 1
+
+    return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('longest_contained_interval.py',
-                                       'longest_contained_interval.tsv',
-                                       longest_contained_range))
+        generic_test.generic_test_main(
+            "longest_contained_interval.py",
+            "longest_contained_interval.tsv",
+            longest_contained_range,
+        )
+    )
