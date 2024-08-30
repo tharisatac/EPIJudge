af25fb0a9ed61aa4b5f911b478a23652808d2719
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-08-30 20:55:57 +0100

    13.1

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -2,14 +2,35 @@ from typing import List
 
 from test_framework import generic_test
 
+""" 13.1 """
+
 
 def intersect_two_sorted_arrays(A: List[int], B: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    """Compute the intersection of two arrays"""
+
+    # Use 2 pointers to do this.
+
+    i, j, intersection = 0, 0, []
+
+    while i < len(A) and j < len(B):
+        if A[i] == B[j]:
+            if A[i] not in intersection:
+                intersection.append(A[i])
+            i += 1
+            j += 1
+        elif A[i] < B[j]:
+            i += 1
+        else:
+            j += 1
+
+    return intersection
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('intersect_sorted_arrays.py',
-                                       'intersect_sorted_arrays.tsv',
-                                       intersect_two_sorted_arrays))
+        generic_test.generic_test_main(
+            "intersect_sorted_arrays.py",
+            "intersect_sorted_arrays.tsv",
+            intersect_two_sorted_arrays,
+        )
+    )
