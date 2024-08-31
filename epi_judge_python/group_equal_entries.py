d1c5b75630a8411fabe42d4b9ba704ec50b9c2e9
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-08-31 12:05:14 +0100

    13.9

--- epi_judge_python/group_equal_entries.py
+++ epi_judge_python/group_equal_entries.py
@@ -6,12 +6,38 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
-Person = collections.namedtuple('Person', ('age', 'name'))
+Person = collections.namedtuple("Person", ("age", "name"))
+
+""" 13.9 """
 
 
 def group_by_age(people: List[Person]) -> None:
-    # TODO - you fill in here.
-    return
+    """Group by age"""
+
+    # First, get the age_to_count and age_to_offset
+    age_to_count = collections.Counter((person.age for person in people))
+    age_to_offset = {}
+    offset = 0
+    for age, count in age_to_count.items():
+        age_to_offset[age] = offset
+        offset += count
+
+    # Second, iterate through age_to_offset and swap.
+    while age_to_offset:
+        # Creates an iterator over the keys
+        from_age = next(iter(age_to_offset))
+        from_idx = age_to_offset[from_age]
+
+        to_age = people[from_idx].age
+        to_idx = age_to_offset[to_age]
+
+        people[from_idx], people[to_idx] = people[to_idx], people[from_idx]
+
+        age_to_count[to_age] -= 1
+        if age_to_count[to_age]:
+            age_to_offset[to_age] = to_idx + 1
+        else:
+            del age_to_offset[to_age]
 
 
 @enable_executor_hook
@@ -25,26 +51,27 @@ def group_by_age_wrapper(executor, people):
     executor.run(functools.partial(group_by_age, people))
 
     if not people:
-        raise TestFailure('Empty result')
+        raise TestFailure("Empty result")
 
     new_values = collections.Counter()
     new_values.update(people)
     if new_values != values:
-        raise TestFailure('Entry set changed')
+        raise TestFailure("Entry set changed")
 
     ages = set()
     last_age = people[0].age
 
     for x in people:
         if x.age in ages:
-            raise TestFailure('Entries are not grouped by age')
+            raise TestFailure("Entries are not grouped by age")
         if last_age != x.age:
             ages.add(last_age)
             last_age = x.age
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('group_equal_entries.py',
-                                       'group_equal_entries.tsv',
-                                       group_by_age_wrapper))
+        generic_test.generic_test_main(
+            "group_equal_entries.py", "group_equal_entries.tsv", group_by_age_wrapper
+        )
+    )
