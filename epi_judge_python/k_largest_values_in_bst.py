68ed02af83558b5ed663b81c54a346ecd9339a72
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-09-02 14:11:51 +0100

    14.3

--- epi_judge_python/k_largest_values_in_bst.py
+++ epi_judge_python/k_largest_values_in_bst.py
@@ -3,15 +3,31 @@ from typing import List
 from bst_node import BstNode
 from test_framework import generic_test, test_utils
 
+""" 14.3i """
+
 
 def find_k_largest_in_bst(tree: BstNode, k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    """Do a reverse in-order-traversal (right -> root -> left)"""
+
+    def _helper(tree: BstNode) -> None:
+        if tree and len(k_largest) < k:
+            _helper(tree.right)
+            if len(k_largest) < k:
+                k_largest.append(tree.data)
+                _helper(tree.left)
+
+    k_largest = []
+    _helper(tree)
+
+    return k_largest
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('k_largest_values_in_bst.py',
-                                       'k_largest_values_in_bst.tsv',
-                                       find_k_largest_in_bst,
-                                       test_utils.unordered_compare))
+        generic_test.generic_test_main(
+            "k_largest_values_in_bst.py",
+            "k_largest_values_in_bst.tsv",
+            find_k_largest_in_bst,
+            test_utils.unordered_compare,
+        )
+    )
