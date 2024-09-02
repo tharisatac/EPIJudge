9a0d6ea0fb3b33e83988288f642aead1ae2c6ab0
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-09-02 18:49:38 +0100

    14.4

--- epi_judge_python/lowest_common_ancestor_in_bst.py
+++ epi_judge_python/lowest_common_ancestor_in_bst.py
@@ -7,25 +7,41 @@ from test_framework.binary_tree_utils import must_find_node
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
+""" 14.4 """
+
 
 # Input nodes are nonempty and the key at s is less than or equal to that at b.
 def find_lca(tree: BstNode, s: BstNode, b: BstNode) -> Optional[BstNode]:
-    # TODO - you fill in here.
-    return None
+    """Finds the lowest common ancestor in a BST"""
+
+    while tree.data < s.data or tree.data > b.data:
+        while tree.data < s.data:
+            # if the current node's data is less than that of s (i.e. the first
+            # node), we haven't found the right subtree yet.
+            tree = tree.right
+        while tree.data > b.data:
+            tree = tree.left
+
+    return tree
 
 
 @enable_executor_hook
 def lca_wrapper(executor, tree, s, b):
     result = executor.run(
-        functools.partial(find_lca, tree, must_find_node(tree, s),
-                          must_find_node(tree, b)))
+        functools.partial(
+            find_lca, tree, must_find_node(tree, s), must_find_node(tree, b)
+        )
+    )
     if result is None:
-        raise TestFailure('Result can\'t be None')
+        raise TestFailure("Result can't be None")
     return result.data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('lowest_common_ancestor_in_bst.py',
-                                       'lowest_common_ancestor_in_bst.tsv',
-                                       lca_wrapper))
+        generic_test.generic_test_main(
+            "lowest_common_ancestor_in_bst.py",
+            "lowest_common_ancestor_in_bst.tsv",
+            lca_wrapper,
+        )
+    )
