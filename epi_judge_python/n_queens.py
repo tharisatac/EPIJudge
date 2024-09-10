a4251c25aea817c93e79f136e2dd8dcb82136b01
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-09-10 21:16:16 +0100

    15.3

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -2,17 +2,36 @@ from typing import List
 
 from test_framework import generic_test
 
+""" 15.3 """
+
 
 def n_queens(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    """Find all non-attacking placements of n-queens"""
+
+    def _solve(row):
+        if row == n:
+            # We've now solved it for theses sets of columns.
+            return result.append(col_placement.copy())
+
+        for col in range(n):
+            if all(
+                abs(c - col) not in (0, row - i)
+                for i, c in enumerate(col_placement[:row])
+            ):
+                col_placement[row] = col
+                _solve(row + 1)
+
+        return
+
+    result: List[List[int]] = []
+    col_placement = [0] * n
+    _solve(0)
+    return result
 
 
 def comp(a, b):
     return sorted(a) == sorted(b)
 
 
-if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('n_queens.py', 'n_queens.tsv', n_queens,
-                                       comp))
+if __name__ == "__main__":
+    exit(generic_test.generic_test_main("n_queens.py", "n_queens.tsv", n_queens, comp))
