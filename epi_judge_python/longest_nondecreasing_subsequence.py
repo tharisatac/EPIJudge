045651131fe7b9b5d13cfc1392e9261c47d19cb8
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-09-22 16:33:48 +0100

    16.12

--- epi_judge_python/longest_nondecreasing_subsequence.py
+++ epi_judge_python/longest_nondecreasing_subsequence.py
@@ -2,15 +2,27 @@ from typing import List
 
 from test_framework import generic_test
 
+""" 16.12 """
+
 
 def longest_nondecreasing_subsequence_length(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    """Find the longest non-decreasing subsequent length"""
+
+    max_length = [1] * len(A)
+
+    for i in range(1, len(A)):
+        max_length[i] = 1 + max(
+            (max_length[j] for j in range(i) if A[i] >= A[j]), default=0
+        )
+
+    return max(max_length)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
         generic_test.generic_test_main(
-            'longest_nondecreasing_subsequence.py',
-            'longest_nondecreasing_subsequence.tsv',
-            longest_nondecreasing_subsequence_length))
+            "longest_nondecreasing_subsequence.py",
+            "longest_nondecreasing_subsequence.tsv",
+            longest_nondecreasing_subsequence_length,
+        )
+    )
