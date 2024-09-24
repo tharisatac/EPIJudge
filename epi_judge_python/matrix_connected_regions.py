00f7a2aacd0210bb5a4f2f734cf437ea2b2d7083
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-09-24 21:39:23 +0100

    18.2

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -1,11 +1,28 @@
 from typing import List
+import collections
 
 from test_framework import generic_test
 
+""" 18.2 """
+
 
 def flip_color(x: int, y: int, image: List[List[bool]]) -> None:
-    # TODO - you fill in here.
-    return
+    """Do BFS"""
+    colour = image[x][y]
+    q = collections.deque([(x, y)])
+
+    image[x][y] = not image[x][y]
+    while q:
+        x, y = q.popleft()
+        for next_x, next_y in ((x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)):
+            if (
+                0 <= next_x < len(image)
+                and 0 <= next_y < len(image[next_x])
+                and image[next_x][next_y] == colour
+            ):
+                # Flip the colour
+                image[next_x][next_y] = not image[next_x][next_y]
+                q.append((next_x, next_y))
 
 
 def flip_color_wrapper(x, y, image):
@@ -13,7 +30,9 @@ def flip_color_wrapper(x, y, image):
     return image
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('matrix_connected_regions.py',
-                                       'painting.tsv', flip_color_wrapper))
+        generic_test.generic_test_main(
+            "matrix_connected_regions.py", "painting.tsv", flip_color_wrapper
+        )
+    )
