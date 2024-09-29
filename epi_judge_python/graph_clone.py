0a7be48aba3cee1b4101a70831dc8a03987af26c
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2024-09-29 12:34:34 +0100

    18.5

--- epi_judge_python/graph_clone.py
+++ epi_judge_python/graph_clone.py
@@ -4,16 +4,34 @@ from typing import List
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+""" 18.5 """
+
 
 class GraphVertex:
     def __init__(self, label: int) -> None:
         self.label = label
-        self.edges: List['GraphVertex'] = []
+        self.edges: List["GraphVertex"] = []
 
 
 def clone_graph(graph: GraphVertex) -> GraphVertex:
-    # TODO - you fill in here.
-    return GraphVertex(0)
+    """Clone a graph, starting from graph"""
+    if graph is None:
+        return
+
+    q = collections.deque([graph])
+    vertex_map = {graph: GraphVertex(graph.label)}
+
+    while q:
+        v = q.popleft()
+        for e in v.edges:
+            # Try to copy the vertex e
+            if e not in vertex_map:
+                vertex_map[e] = GraphVertex(e.label)
+                q.append(e)
+            # Copyu edge
+            vertex_map[v].edges.append(vertex_map[e])
+
+    return vertex_map[graph]
 
 
 def copy_labels(edges):
@@ -22,7 +40,7 @@ def copy_labels(edges):
 
 def check_graph(node, graph):
     if node is None:
-        raise TestFailure('Graph was not copied')
+        raise TestFailure("Graph was not copied")
 
     vertex_set = set()
     q = collections.deque()
@@ -31,11 +49,11 @@ def check_graph(node, graph):
     while q:
         vertex = q.popleft()
         if vertex.label >= len(graph):
-            raise TestFailure('Invalid vertex label')
+            raise TestFailure("Invalid vertex label")
         label1 = copy_labels(vertex.edges)
         label2 = copy_labels(graph[vertex.label].edges)
         if sorted(label1) != sorted(label2):
-            raise TestFailure('Edges mismatch')
+            raise TestFailure("Edges mismatch")
         for e in vertex.edges:
             if e not in vertex_set:
                 vertex_set.add(e)
@@ -44,19 +62,21 @@ def check_graph(node, graph):
 
 def clone_graph_test(k, edges):
     if k <= 0:
-        raise RuntimeError('Invalid k value')
+        raise RuntimeError("Invalid k value")
     graph = [GraphVertex(i) for i in range(k)]
 
-    for (fr, to) in edges:
+    for fr, to in edges:
         if fr < 0 or fr >= k or to < 0 or to >= k:
-            raise RuntimeError('Invalid vertex index')
+            raise RuntimeError("Invalid vertex index")
         graph[fr].edges.append(graph[to])
 
     result = clone_graph(graph[0])
     check_graph(result, graph)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('graph_clone.py', 'graph_clone.tsv',
-                                       clone_graph_test))
+        generic_test.generic_test_main(
+            "graph_clone.py", "graph_clone.tsv", clone_graph_test
+        )
+    )
--- epi_judge_python/matrix_enclosed_regions.py
+++ epi_judge_python/matrix_enclosed_regions.py
@@ -1,11 +1,32 @@
 from typing import List
+import collections
 
 from test_framework import generic_test
 
+""" 18.3 """
+
 
 def fill_surrounded_regions(board: List[List[str]]) -> None:
-    # TODO - you fill in here.
-    return
+    """Fill surrounded regions."""
+
+    n, m = len(board), len(board[0])
+
+    # Collect the boundary cells
+    q = collections.deque(
+        [(i, 0) for i in range(n)]
+        + [(i, m - 1) for i in range(n)]  # First and last column
+        + [(0, j) for j in range(m)]
+        + [(n - 1, j) for j in range(m)]  # First and last row
+    )
+
+    while q:
+        x, y = q.popleft()
+        if 0 <= x < n and 0 <= y < m and board[x][y] == "W":
+            # Flip it to equal t
+            board[x][y] = "T"
+            q.extend([(x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)])
+
+    board[:] = [["B" if c != "T" else "W" for c in row] for row in board]
 
 
 def fill_surrounded_regions_wrapper(board):
@@ -13,8 +34,11 @@ def fill_surrounded_regions_wrapper(board):
     return board
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('matrix_enclosed_regions.py',
-                                       'matrix_enclosed_regions.tsv',
-                                       fill_surrounded_regions_wrapper))
+        generic_test.generic_test_main(
+            "matrix_enclosed_regions.py",
+            "matrix_enclosed_regions.tsv",
+            fill_surrounded_regions_wrapper,
+        )
+    )
